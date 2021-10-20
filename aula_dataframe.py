7c1c9f656c53d9dd7b026cee2672664ef8b849fb
João Victor Leite <91909204+JoaoVictorLeitee@users.noreply.github.com>
2021-10-19 22:26:37 -0300

    Update aula_dataframe.py

--- aula_dataframe.py
+++ aula_dataframe.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     dados_concursos = {
         'banca': ['cespe', 'cesgranrio'],
         'instituicao': ['tcu', 'bb'],
-        'saalrio': [2500,3300],
+        'salario': [2500,3300],
         'data da prova': [pd.to_datetime('20/01/2018'),
                             pd.to_datetime('10/01/2021')]
 
